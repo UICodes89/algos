170cda3afc057a8afceba64a6766ab0961c858e7
Manoj Shukla <mshukla@superhifi.com>
2021-10-26 17:07:09 +0100

    number of ways to make change problem added

---
+++ dynamic_programming/Number_of_ways_to_make_change.js
@@ -0,0 +1,2 @@
+// Question: Given an array of distinct positive integer representing coin denomination and single non-negative integer n representing a target
+amount of money, write a program that return the number of ways to make change for that target amount suing a given coin denominations.
